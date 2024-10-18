af6374f261371a6e297e8825c565d61d85b36f5e
GabrielDelusor <gabriel.estudo2020@gmail.com>
2024-10-18 12:05:40 -0300

    Subindo rp03

---
+++ src/main/java/br/sesi/bank/bank_java_jdbc/domain/cliente/DadosCadastroCliente.java
@@ -0,0 +1,11 @@
+package br.sesi.bank.bank_java_jdbc.domain.cliente;
+
+public class DadosCadastroCliente {
+    String nome, cpf, email;
+
+    public DadosCadastroCliente(String nome, String cpf, String email) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+    }
+}
